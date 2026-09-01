bfe96860b0defd2623a453017b4635d46cc1b502
Loaya9779 <loayadel96@gmail.com>
2026-09-02 01:07:57 +0300

    edits

--- config/db.js
+++ config/db.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/bookstore");
+        await mongoose.connect(
+            "mongodb+srv://loayadel96_db_user:NjXsC1SqPUB64odi@cluster0.wiizy7e.mongodb.net/bookstore?appName=Cluster0"
+        );
 
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("MongoDB connection failed:", error.message);
-        process.exit(1);
     }
 };
 
