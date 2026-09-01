361a0a0352453ba1f74b61030c61cf884cab9526
Loaya9779 <loayadel96@gmail.com>
2026-09-02 00:40:44 +0300

    rdit for vercel

--- server.js
+++ server.js
@@ -10,22 +10,16 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-const PORT = 3000;
-
-
 // Connect MongoDB
 connectDB();
-
 
 // Middlewares
 app.use(express.json());
 
 app.use(logger);
 
-
 // Static files
 app.use(express.static("public"));
-
 
 // About
 app.get("/about", (req, res) => {
@@ -35,26 +29,19 @@ app.get("/about", (req, res) => {
     `);
 });
 
-
 // Authentication
 app.use("/", authRoutes);
 
-
 // Books
 app.use("/books", booksRoutes);
-
 
 // 404
 app.use((req, res) => {
     res.status(404).send("<h1>404 - Page Not Found</h1>");
 });
 
-
 // Error Handler
 app.use(errorMiddleware);
 
-
-// Start server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+// Export app for Vercel
+module.exports = app;
